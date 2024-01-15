2b3a9c722ef58b8345de3d9cad466a599c024a04
Sandeep Chinchali <sandeepc@utexas.edu>
2024-01-15 16:09:50 -0600

    edit spring mass damper example

--- lecture_2_state_space_models/spring_mass_damper.py
+++ lecture_2_state_space_models/spring_mass_damper.py
@@ -5,9 +5,9 @@ import control as ct
 
 
 # Parameters defining the system (list)
-m_list = [250.0, 250]           	# system mass
-k_list = [20, 40, 40.0]            	# spring constant
-b_list = [60.0, 40.0, 80.0]         # damping constant
+m_list = [250, 250, 250]               # system mass
+k_list = [20, 40, 40]             # spring constant
+b_list = [20, 40, 80]         # damping constant
 
 # loop through different configurations
 
@@ -15,31 +15,33 @@ step_response_plot = plt.figure()
 initial_condition_plot = plt.figure()
 
 for m, k, b in zip(m_list, k_list, b_list):
-
     # print the parameters
-	print('mass: ', m, 'spring_const: ', k, 'damping: ', b)
+    legend_str = 'mass: ' + str(m) + ', spring: ' + str(k) + ', damping: ' + str(b)
+    print(legend_str)
 
-	# System matrices
-	A = [[0, 1.], [-k/m, -b/m]]
-	B = [[0], [1/m]]
-	C = [[1., 0]]
-	sys = ss(A, B, C, 0)
+    # System matrices
+    A = [[0, 1.], [-k/m, -b/m]]
+    B = [[0], [1/m]]
+    C = [[1., 0]]
+    sys = ss(A, B, C, 0)
 
-	# Step response for the system
-	plt.figure(step_response_plot.number)
-	yout, T = step(sys)
-	plt.plot(T.T, yout.T)
+    # Step response for the system
+    plt.figure(step_response_plot.number)
+    yout, T = step(sys)
+    plt.plot(T.T, yout.T, label=legend_str)
 
-	# initial condition response
-	time_horizon = 100
-	X0 = [5, 0]
-	T,yout = ct.initial_response(sys, T=time_horizon, X0=X0)
-	plt.figure(initial_condition_plot.number)
-	plt.plot(T.T, yout.T)
+    # initial condition response
+    time_horizon = 100
+    X0 = [5, 0]
+    T,yout = ct.initial_response(sys, T=time_horizon, X0=X0)
+    plt.figure(initial_condition_plot.number)
+    plt.plot(T.T, yout.T, label=legend_str)
 
 plt.figure(step_response_plot.number)
+plt.legend()
 plt.savefig('step.pdf')
 
 
 plt.figure(initial_condition_plot.number)
+plt.legend()
 plt.savefig('init.pdf')
