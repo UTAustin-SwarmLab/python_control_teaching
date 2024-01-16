1076ae100b2cd3a0997e497df55e8f358c675b36
Sandeep Chinchali <sandeepc@utexas.edu>
2024-01-15 18:59:19 -0600

    comment the code

--- stable_vs_unstable_control_systems/stability_analysis.py
+++ stable_vs_unstable_control_systems/stability_analysis.py
@@ -1,19 +1,27 @@
+"""
+This code simulates a simple 1D LTI system. It shows the exponential response for unstable and stable systems and contrasts them.
+
+"""
+
 import os
 import matplotlib.pyplot as plt   # MATLAB plotting functions
 from control.matlab import *  # MATLAB-like functions
 import control as ct
 
 
-# Do the scalar case
+# Do the scalar case, exp(-a*t)
 a_list = [-2, -1, -0.5, 0, 0.1]
 
-# loop through different configurations
+# initialize two plots
 step_response_plot = plt.figure()
 initial_condition_plot = plt.figure()
 
+# time horizon to simulate
 time_horizon = 5
+# initial condition
 X0 = [1]
 
+# loop through different stable and unstable configurations
 for a in zip(a_list):
     # print the parameters
     legend_str = 'exp(' + str(a) + 't)'
@@ -35,6 +43,7 @@ for a in zip(a_list):
     plt.figure(initial_condition_plot.number)
     plt.plot(T.T, yout.T, label=legend_str)
 
+# plot in two separate plots
 plt.figure(step_response_plot.number)
 plt.legend()
 if 'CONTROL_PLOT_DIR' not in os.environ:
